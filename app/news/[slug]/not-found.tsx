70706b1751f6e8b10ad34d83f601e32e62c2c235
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-12-16 14:27:39 +0100

    feat: add Not Found pages for general and news sections

---
+++ app/news/[slug]/not-found.tsx
@@ -0,0 +1,13 @@
+const NewsNotFoundPage = () => {
+  return (
+    <div id="error">
+      <h1>Not found</h1>
+      <p>
+        Unfortunately, we couldn&apos;t find the news article you were looking
+        for!
+      </p>
+    </div>
+  );
+};
+
+export default NewsNotFoundPage;
---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div id="error">
+      <h1>Not found</h1>
+      <p>Unfortunately, we couldn&apos;t find the page you were looking for!</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
