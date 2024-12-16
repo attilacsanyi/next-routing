6d41300af0ff238ba74ce97f6c0ef10fab8892e9
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-12-16 15:59:53 +0100

    refactor: replace ArchiveYearPage with a catch-all route for dynamic year and month filtering, enhancing navigation and news retrieval

---
+++ app/archive/@archive/[[...filter]]/page.tsx
@@ -0,0 +1,65 @@
+import NewsList from "@/components/news-list";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/libs/news-dao";
+import { News } from "@/libs/news.types";
+import Link from "next/link";
+
+const ArchiveYearPage = async ({
+  params,
+}: {
+  params: Promise<{ filter: string[] | undefined }>;
+}) => {
+  // Catch all route
+  const { filter } = await params;
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news: News[] | undefined;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(+selectedYear);
+    links = getAvailableNewsMonths(+selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(+selectedYear, +selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for the selected period.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+
+      {newsContent}
+    </>
+  );
+};
+
+export default ArchiveYearPage;
