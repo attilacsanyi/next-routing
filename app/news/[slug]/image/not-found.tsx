f52fd9aa62516938bd57be943f2547027de7b677
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-12-17 11:16:14 +0100

    feat: add ImageNotFoundPage component for handling missing images in news articles

---
+++ app/news/[slug]/image/not-found.tsx
@@ -0,0 +1,12 @@
+const ImageNotFoundPage = () => {
+  return (
+    <div id="error">
+      <h1>Image Not found</h1>
+      <p>
+        Unfortunately, we couldn&apos;t find the image you were looking for!
+      </p>
+    </div>
+  );
+};
+
+export default ImageNotFoundPage;
