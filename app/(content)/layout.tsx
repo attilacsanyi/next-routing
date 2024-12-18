514ab287011af59bb7eba3fac10028c321cfe440
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-12-18 14:42:50 +0100

    feat: add route groups defining landing group without main header versus content group including news and archive pages with main header

---
+++ app/(content)/layout.tsx
@@ -0,0 +1,25 @@
+import MainHeader from "@/components/main-header";
+import type { Metadata } from "next";
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "Next.js Page Routing & Rendering",
+  description: "Learn how to route to different pages.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <div id="page">
+          <MainHeader />
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,6 +1,5 @@
-import MainHeader from "@/components/main-header";
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 
 export const metadata: Metadata = {
   title: "Next.js Page Routing & Rendering",
@@ -14,12 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <div id="page">
-          <MainHeader />
-          {children}
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
